e2e2dba9f050f06c73176a9b71e8b3d0a6dcbc0a
fawwazzzz <72958719+fawwazzzz@users.noreply.github.com>
2022-01-25 14:38:25 +0700

    menambahkan

--- rest-api/app/Http/Controllers/StudentController.php
+++ rest-api/app/Http/Controllers/StudentController.php
@@ -7,8 +7,10 @@ use App\Models\Student;
 
 class StudentController extends Controller
 {
+    # membuat method index
     public function index () 
     {
+        # menggunakan model Student untuk select data
         $students = Student::all();
         $data = [
             'messege' => 'Get All Students' ,
@@ -17,21 +19,107 @@ class StudentController extends Controller
         return response()->json($data, 200);
     }
 
+    # membuat method store
     public function store (Request $request) 
     {
-        $input = [
-            'nama' => $request->nama,
-            'nim' => $request->nim,
-            'email' => $request->email,
-            'jurursan' => $request->jurusan
-        ]; 
-        $student = Student::create($input);
+        # membuat validasi 
+        $validatedData = $request->validate([
+            # kolom => 'rules|rules'
+            'nama' => 'required',
+            'nim' => 'numeric|required',
+            'email' => 'email|required',
+            'jurusan' => 'required'
+        ]);
+ 
+
+        # menggunakan model Student untuk insert data
+        $student = Student::create($validatedData);
 
         $data = [
-            'messege' => 'Student is created',
+            'messege' => 'Student is created succesfully',
             'data' => $student
         ];
 
+        // mengembalikan data (json) dan kode 201
         return response()->json($data, 201);
+    }
+
+    # membuat method show
+    public function show($id)
+    {
+        $student = Student::find($id);
+        
+        if ($student) {
+            $data = 
+            [
+                'messege' => 'Get detail student',
+                'data' => $student
+            ];
+            return response()->json($data, 200);
+        }
+        else {
+            $data = 
+            [
+                'messege' => 'Data not found',
+            ];
+            return response()->json($data, 404);
+        }
+    }
+
+    # membuat method update
+    public function update (Request $request, $id) 
+    {
+        # mencari id student yang ingin di update
+        $student = Student::find($id);
+
+        if ($student) {
+            # menangkap data request
+            $input = [
+                'nama' => $request->nama ?? $student->nama,
+                'nim' => $request->nim ?? $student->nim,
+                'email' => $request->email ?? $student->email,
+                'jurusan' => $request->jurusan ?? $student->jurusan
+            ];
+            # melaakukan update data
+            $student->update($input);
+
+            $data = [
+                'messege' => 'Data is update',
+                'data' => $student
+            ];
+            # mengmbalikan data (json) dan kode 200
+            return response()->json($data, 200);
+        }
+        else {
+            $data = 
+            [
+                'messege' => 'Data not found',
+            ];
+            return response()->json($data, 404);
+        }
+    }
+
+    # membuat method destroy
+    public function destroy($id) {
+        # mencari data yang ingin dihapus
+        $student = Student::find($id);
+
+        if ($student) {
+            # hapus data student
+            $student->delete();
+
+            $data = [
+                'messege' => 'Data is deleted'
+            ];
+            # mengmbalikan data (json) dan kode 200
+            return response()->json($data, 200);
+        }
+
+        else {
+            $data = [
+                'messege' => 'Data not found'
+            ];
+            return response()->json($data, 404);
+        }
     }
 }
