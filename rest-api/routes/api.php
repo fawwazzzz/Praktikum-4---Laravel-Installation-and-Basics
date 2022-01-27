1b5b9f0d8868c0b38cdce6ae77937ca95bc52f14
fawwazzzz <72958719+fawwazzzz@users.noreply.github.com>
2022-01-27 11:09:47 +0700

    api

---
+++ rest-api/routes/api.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\StatusController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+# Get All Patient
+Route::get("/patients", [PatientController::class, 'index']);
+# Get Detail Patient
+Route::get("/patients/{id}", [PatientController::class, 'show']);
+# Add Patient
+Route::post("/patients", [PatientController::class, 'store']);
+# Update patient
+Route::put("/patients", [PatientController::class, 'update']);
+# Delete Patient
+Route::delete("/patients", [PatientController::class, 'destroy']);
+
+# Get Patient by Name
+Route::get("/patients/search/{name}", [PatientController::class, 'search']);
+# Get Patient Positive
+Route::get("/patients/status/positive", [PatientController::class, 'positive']);
+# Get Patient Recovered
+Route::get("/patients/status/recovered", [PatientController::class, 'recovered']);
+# Get Patient Dead
+Route::get("/patients/status/dead", [PatientController::class, 'dead']);
+
+# Get Status
+Route::get("/status", [StatusController::class, 'status']);
+
+
+# Endpoint Register n Login
+Route::post ('/register', [AuthController::class, 'register']);
+Route::post ('/login', [AuthController::class, 'login']);
+
+
+
